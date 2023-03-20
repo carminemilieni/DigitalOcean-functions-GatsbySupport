dadf96648287b8cefb8cab5bbaa376c858001ece
Carmine Milieni <carmine.milieni@gmail.com>
2023-03-20 20:21:22 +0100

    Add GitHub last three repos function

---
+++ packages/github/getLastThreeRepos/getLastThreeRepos.js
@@ -0,0 +1,44 @@
+const {Octokit} = require("@octokit/rest");
+
+async function main() {
+    const octokit = new Octokit();
+    const username = process.env.GITHUB_USERNAME;
+
+    try {
+        const response = await octokit.rest.repos
+            .listForUser({
+                username,
+                per_page: 3,
+                sort: 'updated'
+            })
+
+        let {data} = response;
+
+        const body = data.map(item => ({
+            id: item.id,
+            name: item.name,
+            description: item.description,
+            url: item.html_url,
+        }))
+
+        return {
+            body
+        };
+
+    }  catch (e) {
+        const {response} = e;
+        if (response && response.data) {
+            return {
+                body: response.data,
+                statusCode: response.status
+            };
+        } else {
+            throw new Error(e);
+        }
+
+    }
+
+
+}
+
+exports.main = main;
